92eb7c06cd20f53b763c72f61b549eecf9f8a0a7
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-09-17 03:25:23 +0000

    set admins

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,12 @@
-class hoodie {
+class hoodie (
+  $admins = hiera('hoodie::admins', {})
+) {
   include hoodie::user
   include hoodie::install
 
   couchdb { 'couchdb-hoodie':
     port     => 5986,
     ssl_port => 6986,
+    admins   => $admins,
   }
 }
